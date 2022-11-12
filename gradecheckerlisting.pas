127c58b64820bb45598e666c29d33e5989ea8e4f
Rafli Raihan <115336466+rafli-raihan@users.noreply.github.com>
2022-11-12 20:06:15 +0700

    Add files via upload

--- gradecheckerlisting.pas
+++ gradecheckerlisting.pas
@@ -14,6 +14,11 @@ type
   TForm1 = class(TForm)
     ButtonCheck: TButton;
     ButtonClear: TButton;
+    EditNama: TEdit;
+    EditKelas: TEdit;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
     NilaiPTS: TEdit;
     NilaiUAS: TEdit;
     Label1: TLabel;
@@ -47,23 +52,25 @@ begin
   strAkhir:= 'Nilai akhir:';
   strGrade:= 'Grade: ';
   if akhir >= 94
-  then showmessage (strAkhir + sLineBreak + Floattostr (akhir) + sLineBreak + sLineBreak + strGrade + sLineBreak + 'A')
+  then showmessage (EditNama.Text + sLineBreak + EditKelas.Text + sLineBreak + sLineBreak + strAkhir + sLineBreak + Floattostr (akhir) + sLineBreak + sLineBreak + strGrade + sLineBreak + 'A')
   else
   if akhir >= 84
-  then showmessage (strAkhir + sLineBreak + Floattostr (akhir) + sLineBreak + sLineBreak + strGrade + sLineBreak + 'B')
+  then showmessage (EditNama.Text + sLineBreak + EditKelas.Text + sLineBreak + sLineBreak + strAkhir + sLineBreak + Floattostr (akhir) + sLineBreak + sLineBreak + strGrade + sLineBreak + 'B')
   else
   if akhir >= 74
-  then showmessage (strAkhir + sLineBreak + Floattostr (akhir) + sLineBreak + sLineBreak + strGrade + sLineBreak + 'C')
+  then showmessage (EditNama.Text + sLineBreak + EditKelas.Text + sLineBreak + sLineBreak + strAkhir + sLineBreak + Floattostr (akhir) + sLineBreak + sLineBreak + strGrade + sLineBreak + 'C')
   else
   if akhir >= 63
-  then showmessage (strAkhir + sLineBreak + Floattostr (akhir) + sLineBreak + sLineBreak + strGrade + sLineBreak + 'D')
-  else showmessage (strAkhir + sLineBreak + Floattostr (akhir) + sLineBreak + sLineBreak + strGrade + sLineBreak + 'E')
+  then showmessage (EditNama.Text + sLineBreak + EditKelas.Text + sLineBreak + sLineBreak + strAkhir + sLineBreak + Floattostr (akhir) + sLineBreak + sLineBreak + strGrade + sLineBreak + 'D')
+  else showmessage (EditNama.Text + sLineBreak + EditKelas.Text + sLineBreak + sLineBreak + strAkhir + sLineBreak + Floattostr (akhir) + sLineBreak + sLineBreak + strGrade + sLineBreak + 'E')
 end;
 
 procedure TForm1.ButtonClearClick(Sender: TObject);
 begin
   NilaiPTS.Clear;
   NilaiUAS.Clear;
+  EditNama.Clear;
+  EditKelas.Clear;
 end;
 
 end.
